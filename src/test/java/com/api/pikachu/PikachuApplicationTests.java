866a70bacd5a13189033a0f2967ee085f4334c1b
Vishali.Ba <vishali.bagga21@gmail.com>
2023-01-19 14:45:48 -0500

    first commit

---
+++ src/test/java/com/api/pikachu/PikachuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pikachu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikachuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
